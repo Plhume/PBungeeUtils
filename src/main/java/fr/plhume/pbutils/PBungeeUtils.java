184688e0593808749a71f077f6afdbed5e5d2e6a
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-24 17:54:03 +0100

    Initial commit

---
+++ src/main/java/fr/plhume/pbutils/PBungeeUtils.java
@@ -0,0 +1,16 @@
+package fr.plhume.pbutils;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class PBungeeUtils extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
