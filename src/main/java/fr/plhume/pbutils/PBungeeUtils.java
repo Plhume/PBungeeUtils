15a758838f2aa79a94442cd181c649a4c1d3a91a
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-25 14:53:22 +0100

    Update PBungeeUtils.java

--- src/main/java/fr/plhume/pbutils/PBungeeUtils.java
+++ src/main/java/fr/plhume/pbutils/PBungeeUtils.java
@@ -22,4 +22,8 @@ public final class PBungeeUtils extends Plugin {
     public void onDisable() {
         getProxy().getLogger().info("Plugin disabled!");
     }
+
+    public PluginConfig getPluginConfig() {
+        return pConfig;
+    }
 }
