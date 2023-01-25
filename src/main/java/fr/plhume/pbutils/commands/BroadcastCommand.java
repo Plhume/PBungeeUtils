1782f3a7faa0adb29de868c4917df20ca49132bf
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-25 15:00:30 +0100

    Creating basics functions

--- src/main/java/fr/plhume/pbutils/commands/BroadcastCommand.java
+++ src/main/java/fr/plhume/pbutils/commands/BroadcastCommand.java
@@ -1,7 +1,18 @@
 package fr.plhume.pbutils.commands;
 
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
 /**
  * @author Plhume
  */
-public class BroadcastCommand {
+public class BroadcastCommand extends Command {
+    public BroadcastCommand(String name) {
+        super(name);
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+    }
 }
