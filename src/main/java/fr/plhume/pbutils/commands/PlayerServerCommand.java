23c40e25751ecf28b7ca5464458900f6beb7890c
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-25 16:46:21 +0100

    Update PlayerServerCommand.java

--- src/main/java/fr/plhume/pbutils/commands/PlayerServerCommand.java
+++ src/main/java/fr/plhume/pbutils/commands/PlayerServerCommand.java
@@ -26,6 +26,17 @@ public class PlayerServerCommand extends Command {
             sender.sendMessage(new TextComponent(ChatColor.RED + "Merci d'introduire des arguments dans votre commande !"));
             return;
         }
+
+        ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
+
+        if (target == null) {
+            sender.sendMessage(new TextComponent(ChatColor.RED + "Le joueur " + ChatColor.WHITE + args[0] + ChatColor.RED + " n'existe pas ou n'est pas connecté !"));
+            return;
+        } else {
+            sender.sendMessage(new TextComponent(ChatColor.GOLD + "Informations du joueur >" +
+                    ChatColor.GOLD + "\nNom : " + ChatColor.WHITE + target.getDisplayName() +
+                    ChatColor.GOLD + "\nConnecté sur : " + ChatColor.WHITE + target.getServer().getInfo().getName() +
+                    ChatColor.GOLD + "\nPing : " + ChatColor.WHITE + target.getPing()));
         }
     }
 }
